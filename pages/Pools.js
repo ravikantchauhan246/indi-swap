61ed77292effe3457b7b2f456e0c19478f7c236d
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2024-10-01 20:52:38 +0530

    ui improvements

--- pages/Pools.js
+++ pages/Pools.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Pools = () => {
-  return <div>Pools</div>;
+  return <div></div>;
 };
 
 export default Pools;
--- pages/Tokens.js
+++ pages/Tokens.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Tokens = () => {
-  return <div>Tokens</div>;
+  return <div></div>;
 };
 
 export default Tokens;
